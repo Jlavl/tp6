397091aede90a346b309602c3c66653d6c0c81b4
Julian <jelaverallauro@gmail.com>
2024-07-31 10:18:13 -0300

    ehehe

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Alert } from 'react-native';
+import { Button } from 'react-native-web';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text> Este es un titulo.</Text>
+      <Button
+      title='presioname'
+      onPress={() => Alert.alert('Se ha presionado!')}
+      />
     </View>
   );
 }
